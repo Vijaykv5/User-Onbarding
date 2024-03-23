5a7ebfa63df053203c32c8ec35a5441203d25642
vijay <vijaykv2228@gmail.com>
2024-03-24 00:41:17 +0530

    Last step

--- app/page.js
+++ app/page.js
@@ -17,27 +17,35 @@ export default function Component() {
     setJoyrideSteps([
       {
         target: ".bs-cart-button",
-        title:"Orders",
-        content: "Here you'll find all the items in your cart for this patient.",
-        placement:"left",
+        title: "Orders",
+        content:
+          "Here you'll find all the items in your cart for this patient.",
+        placement: "left",
         disableBeacon: true,
       },
       {
         target: ".ri-pencil-button",
-        content: "This is the pencil button.",
+        title: "Visit note",
+        content: "This section is for adding visit notes for the patient.",
+        placement: "left",
         disableBeacon: true,
       },
       {
         target: ".io-document-button",
-        content: "This is the document button.",
+        title: "Clinical Forms",
+        content:
+          "Here you can add your own clinical forms and search from the list of forms.",
+        placement: "bottom",
         disableBeacon: true,
       },
       {
         target: ".gr-group-button",
-        content: "This is the group button.",
+        title: "List of patients",
+        content:
+          "This section contains the list of patients you have visited recently.",
+       
         // disableBeacon: true,
       },
-      
     ]);
   }, []);
 
@@ -49,8 +57,10 @@ export default function Component() {
             OpenMRS
           </div>
           <div className="flex  flex-grow  mr-[400px] text-sm text-gray-100 font-semibold">
-            Vijay KV <p className="text-gray-400 ml-2"> 21 yrs, Male</p>
+            Vijay KV
+            <p className="text-gray-400 ml-2"> 21 yrs, Male</p>
           </div>
+
           <div className="flex justify-end align-bottom">
             <Button
               className="ml-auto rounded-sm text-white px-8 bg-[#027D78]"
@@ -116,13 +126,13 @@ export default function Component() {
                 Conditions
               </Link>
               <Link
-                className="flex items-center gap-2 text-sm font-medium p-2 rounded-md hover:bg-gray-50 hover:text-black dark:hover:bg-gray-100"
+                className="flex items-center gap-2 text-sm font-medium p-2 rounded-md  hover:bg-gray-50 hover:text-black dark:hover:bg-gray-100"
                 href="#"
               >
                 Immunizations
               </Link>
               <Link
-                className="flex items-center gap-2 text-sm font-medium p-2 rounded-md hover:bg-gray-50 hover:text-black dark:hover:bg-gray-100"
+                className="flex items-center gap-2 text-sm font-medium p-2 rounded-md bg-gray-200 hover:bg-gray-300 hover:text-black dark:hover:bg-gray-300"
                 href="#"
               >
                 Attachments
@@ -143,7 +153,31 @@ export default function Component() {
           </div>
         </div>
         <div className="flex-1 flex flex-col">
-          {/* Content for the middle section */}
+          <div className="patient-name    w-[100%] h-40 ">
+            <div className="p-7 flex ">
+              <div className="w-20 h-20 bg-blue-400"></div>
+              <div className="flex  flex-col ">
+                <p className=" font-light text-xl ml-2">Vijay KV</p>
+                <p className="m-2">Male · 21 yrs · 16 — Oct — 2003</p>
+                <p className="text-black bg-slate-300 w-fit p-2 rounded-3xl ml-2 text-sm ">
+                  OpenMRS ID
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="attachments  border border-b-2 border-gray-200 p-24 w-full h-96 flex justify-center items-center">
+            <div class="w-full h-60 p-3 flex flex-col">
+              <div class="flex justify-start mb-2">
+                <p className="font-semibold text-lg">Attachments</p>
+              </div>
+              <div class="flex justify-center items-center h-full">
+                There are no attachments to display for this patient
+              </div>
+              <button className="flex justify-center items-center text-blue-300">
+                Record Attachments
+              </button>
+            </div>
+          </div>
         </div>
         <div className="flex w-14 items-start border-l">
           <div className="flex-1 flex flex-col ">
